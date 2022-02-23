a50238e67807ad5400e3566428579b2f49a5662e
tbekpro <tbekpro@gmail.com>
2022-02-23 23:32:03 +0500

    - solution for https://leetcode.com/submissions/detail/647583201/?from=explore&item_id=2380

--- src/LeetCode/Recursion/RecursionPower.java
+++ src/LeetCode/Recursion/RecursionPower.java
@@ -2,13 +2,14 @@ package LeetCode.Recursion;
 
 public class RecursionPower {
     public static void main(String[] args) {
-        System.out.println(myPow(0.00001, 2147483647));
-        System.out.println(myPow(2, 3));
+        System.out.println(myPow2(2, -2147483647));
+        /*System.out.println(myPow2(0.00001, 2147483647));
+        System.out.println(myPow2(2, 3));
+        System.out.println(myPow2(2, 10));
         System.out.println(myPow(2, 4));
-        System.out.println(myPow(2, -2));
         System.out.println(myPow(2, -3));
         System.out.println(myPow(2, -8));
-        System.out.println(myPow(3, 3));
+        System.out.println(myPow(3, 3));*/
     }
     static double initial = 0;
 
@@ -41,5 +42,43 @@ public class RecursionPower {
         } else {
             return myPowHelper((x * (1 / initial)), n + 1);
         }
+    }
+
+    public static double myPow2(double x, int n) {
+        initial = x;
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n == -1) {
+            return 1 / x;
+        }
+        if (x == 1.0) {
+            return x;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        if (x == -1.0) {
+            if (n % 2 == 0) {
+                return -x;
+            } else {
+                return x;
+            }
+
+        }
+        double result = 1.0;
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                result *= x;
+            }
+        } else {
+            for (int i = 0; i < (-n) ; i++) {
+                result *= 1 / x;
+            }
+        }
+        return result;
     }
 }

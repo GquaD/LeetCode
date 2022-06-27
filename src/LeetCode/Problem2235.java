2c210a2ccbba526451343a69ec68691b3d8af934
tbekpro <tbekpro@gmail.com>
2022-06-28 01:49:32 +0500

    //solution for https://leetcode.com/problems/add-two-integers/

---
+++ src/LeetCode/Problem2235.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class Problem2235 {
+    //solution for https://leetcode.com/problems/add-two-integers/
+    public static void main(String[] args) {
+
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}

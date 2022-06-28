cedb78a4f914f7747d380e9c920d25635c16af9b
tbekpro <tbekpro@gmail.com>
2022-06-29 01:24:37 +0500

    - solved 2 problems

---
+++ src/LeetCode/Problem2011.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class Problem2011 {
+    //https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+    public static void main(String[] args) {
+
+    }
+
+    public static int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].contains("+")) {
+                result++;
+            } else {
+                result--;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/LeetCode/Problem2236.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class Problem2236 {
+    //https://leetcode.com/problems/root-equals-sum-of-children/
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean checkTree(TreeNode root) {
+        return root.val == root.left.val + root.right.val;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

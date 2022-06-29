483c18a886526212a6a114e1846a380354954725
tbekpro <tbekpro@gmail.com>
2022-06-30 01:16:53 +0500

    https://leetcode.com/problems/create-target-array-in-the-given-order/

---
+++ src/LeetCode/Problem1389.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problem1389 {
+    //https://leetcode.com/problems/create-target-array-in-the-given-order/
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            index[i] = list.get(i);
+        }
+        return index;
+    }
+}

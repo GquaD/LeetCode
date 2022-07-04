d873f802c7a0e0d4317181c3aae59289c87ffed7
tbekpro <tbekpro@gmail.com>
2022-07-05 01:41:23 +0500

    //https://leetcode.com/problems/to-lower-case/

--- src/LeetCode/Problem2220.java
+++ src/LeetCode/Problem2220.java
@@ -3,9 +3,9 @@ package LeetCode;
 public class Problem2220 {
     //https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
     public static void main(String[] args) {
-        System.out.println(minBitFlips(10, 7));
-        System.out.println(minBitFlips(3, 4));
-        System.out.println(minBitFlips(10, 82));
+        System.out.println(minBitFlips2(10, 7));
+        System.out.println(minBitFlips2(3, 4));
+        System.out.println(minBitFlips2(10, 82));
     }
 
     //Runtime: 2 ms, faster than 16.18% of Java online submissions for Minimum Bit Flips to Convert Number.
@@ -31,14 +31,16 @@ public class Problem2220 {
         return count;
     }
 
-    public int minBitFlips1(int start, int goal) {
-        int xor =start ^ goal;
-        int count=0;
-        while(xor>0){
-            count++;
-            xor=xor & (xor-1);
+    //Runtime: 2 ms, faster than 16.18% of Java online submissions for Minimum Bit Flips to Convert Number.
+    //Memory Usage: 40.9 MB, less than 57.42% of Java online submissions for Minimum Bit Flips to Convert Number.
+    public static int minBitFlips2(int start, int goal) {
+        String s = Integer.toBinaryString(start ^ goal);
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                count++;
+            }
         }
         return count;
-
     }
 }

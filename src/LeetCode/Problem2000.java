c8ba0eeb8b0baf98e8bd5d786932bda5349340aa
tbekpro <tbekpro@gmail.com>
2022-07-06 02:24:50 +0500

    https://leetcode.com/problems/reverse-prefix-of-word/

---
+++ src/LeetCode/Problem2000.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+import LeetCode.Recursion.ReverseString;
+
+public class Problem2000 {
+    //https://leetcode.com/problems/reverse-prefix-of-word/
+    public static void main(String[] args) {
+        System.out.println(reversePrefix("abcdefd", 'd'));
+        System.out.println(reversePrefix("xyxzxe", 'z'));
+        System.out.println(reversePrefix("abcd", 'z'));
+    }
+    //Runtime: 3 ms, faster than 26.17% of Java online submissions for Reverse Prefix of Word.
+    //Memory Usage: 42.1 MB, less than 60.79% of Java online submissions for Reverse Prefix of Word.
+    public static String reversePrefix(String word, char ch) {
+        if (!word.contains(ch + "")) {
+            return word;
+        }
+
+        int index = word.indexOf(ch) + 1;
+        String toRevert = word.substring(0, index);
+        String reverted = revert(toRevert);
+        return reverted + word.substring(index);
+    }
+
+    static String revert(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}

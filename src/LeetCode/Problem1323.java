043cbcb2ea4404b3a64c4c738e51281dab30ded8
tbekpro <tbekpro@gmail.com>
2022-07-14 01:35:33 +0500

    https://leetcode.com/problems/maximum-69-number/ - drastically improved performance (by 9 ms)

--- src/LeetCode/Problem1323.java
+++ src/LeetCode/Problem1323.java
@@ -6,6 +6,24 @@ public class Problem1323 {
         System.out.println(maximum69Number(9669));
         System.out.println(maximum69Number(9996));
         System.out.println(maximum69Number(9999));
+        System.out.println("/////////////////");
+        System.out.println(maximum69Number1(9669));
+        System.out.println(maximum69Number1(9996));
+        System.out.println(maximum69Number1(9999));
+    }
+
+    //Runtime: 1 ms, faster than 87.40% of Java online submissions for Maximum 69 Number.
+    //Memory Usage: 41.5 MB, less than 18.31% of Java online submissions for Maximum 69 Number.
+    static int maximum69Number1(int num) {
+        char[] arr = (num + "").toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '6') {
+                arr[i] = '9';
+                break;
+            }
+        }
+        String s = String.copyValueOf(arr);
+        return Integer.parseInt(s);
     }
 
     //Runtime: 10 ms, faster than 11.88% of Java online submissions for Maximum 69 Number.

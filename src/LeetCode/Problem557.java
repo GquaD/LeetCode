4c6cbd622f27437bddb4bf4c92a17bef283c4588
tbekpro <tbekpro@gmail.com>
2022-07-14 00:35:13 +0500

    https://leetcode.com/problems/reverse-words-in-a-string-iii/

---
+++ src/LeetCode/Problem557.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class Problem557 {
+    //https://leetcode.com/problems/reverse-words-in-a-string-iii/
+    public static void main(String[] args) {
+        System.out.println(reverseWords("Let's take LeetCode contest"));
+        System.out.println(reverseWords("s'teL ekat edoCteeL tsetnoc"));
+        System.out.println(reverseWords("God Ding"));
+    }
+
+    //Runtime: 10 ms, faster than 61.13% of Java online submissions for Reverse Words in a String III.
+    //Memory Usage: 52 MB, less than 41.70% of Java online submissions for Reverse Words in a String III.
+    static String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            sb.append(reverse(words[i], sb1)).append(" ");
+            sb1.setLength(0);
+        }
+        return sb.toString().trim();
+    }
+    private static String reverse(String word, StringBuilder sb) {
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(word.charAt(word.length() - 1 - i));
+        }
+        return sb.toString();
+    }
+}

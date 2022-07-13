dab2ded4c812712074b81c7b7cce02a6e981f1b2
tbekpro <tbekpro@gmail.com>
2022-07-14 00:22:40 +0500

    https://leetcode.com/problems/replace-all-digits-with-characters/ - performance improvement

--- src/LeetCode/Problem1844.java
+++ src/LeetCode/Problem1844.java
@@ -6,13 +6,29 @@ public class Problem1844 {
         System.out.println(replaceDigits("a1c1e1"));
         System.out.println(replaceDigits("a1b2c3d4e"));
     }
+    //Runtime: 1 ms, faster than 82.54% of Java online submissions for Replace All Digits with Characters.
+    //Memory Usage: 42.8 MB, less than 18.35% of Java online submissions for Replace All Digits with Characters.
+    static String replaceDigits1(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < s.length(); i += 2) {
+            char c = shift1(s.charAt(i - 1), s.charAt(i) - 48);
+            sb.append(s.charAt(i - 1)).append(c);
+        }
+        if (s.length() % 2 == 1) {
+            sb.append(s.charAt(s.length() - 1));
+        }
+        return sb.toString();
+    }
 
+    private static char shift1(char charAt, int num) {
+        return (char) (charAt + num);
+    }
     //Runtime: 3 ms, faster than 30.65% of Java online submissions for Replace All Digits with Characters.
     //Memory Usage: 42.5 MB, less than 27.68% of Java online submissions for Replace All Digits with Characters.
     static String replaceDigits(String s) {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < s.length(); i += 2) {
-            char c = shift(s.charAt(i - 1), Integer.parseInt(s.charAt(i) + ""));
+            char c = shift(s.charAt(i - 1), s.charAt(i) - 48);
             sb.append(s.charAt(i - 1)).append(c);
         }
         if (s.length() % 2 == 1) {

7dfe063b350dda6b981a8a2a105c681841589406
tbekpro <tbekpro@gmail.com>
2022-07-27 22:06:45 +0500

    https://leetcode.com/problems/counting-bits/ - easy level

--- src/LeetCode/Problem338.java
+++ src/LeetCode/Problem338.java
@@ -10,9 +10,28 @@ public class Problem338 {
         System.out.println(Arrays.toString(countBits(10)));
     }
 
+    //Runtime: 23 ms, faster than 6.77% of Java online submissions for Counting Bits.
+    //Memory Usage: 48.3 MB, less than 39.93% of Java online submissions for Counting Bits.
+    static int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = count1sInBinaryOf(i);
+        }
+        return res;
+    }
+
+    static int count1sInBinaryOf(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n % 2 == 0 ? 0 : 1;
+            n /= 2;
+        }
+        return count;
+    }
+
     //Runtime: 16 ms, faster than 13.36% of Java online submissions for Counting Bits.
     //Memory Usage: 53.5 MB, less than 5.02% of Java online submissions for Counting Bits.
-    static int[] countBits(int n) {
+    static int[] countBits1(int n) {
         int[] res = new int[n + 1];
         for (int i = 0; i < res.length; i++) {
             String temp = Integer.toBinaryString(i);

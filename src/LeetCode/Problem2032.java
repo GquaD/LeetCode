02f5b03ffc3ec4c1096f2b4c9d971610ff1c9444
tbekpro <tbekpro@gmail.com>
2022-07-30 00:47:34 +0500

    https://leetcode.com/problems/two-out-of-three/ - easy level

--- src/LeetCode/Problem2032.java
+++ src/LeetCode/Problem2032.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem2032 {
     //https://leetcode.com/problems/two-out-of-three/
@@ -9,11 +10,68 @@ public class Problem2032 {
         System.out.println(twoOutOfThree(new int[]{3, 1}, new int[]{2, 3}, new int[]{1, 2}));
     }
 
+    //Runtime: 9 ms, faster than 51.57% of Java online submissions for Two Out of Three.
+    //Memory Usage: 46.6 MB, less than 62.68% of Java online submissions for Two Out of Three.
+    static List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        Set<Integer> set = new HashSet<>(); //or use set
+        findInOtherArrays(nums1, nums2, nums3, set);
+//        findInOtherArrays(nums2, nums1, nums3, set);
+//        findInOtherArrays(nums3, nums2, nums1, set);
+        for (int i = 0; i < nums2.length; i++) {
+            for (int j = 0; j < nums3.length; j++) {
+                if (nums2[i] == nums3[j]) {
+                    set.add(nums2[i]);
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+    private static void findInOtherArrays(int[] nums1, int[] nums2, int[] nums3, Set<Integer> set) {
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    set.add(nums1[i]);
+                }
+            }
+            for (int j = 0; j < nums3.length; j++) {
+                if (nums1[i] == nums3[j]) {
+                    set.add(nums1[i]);
+                }
+            }
+        }
+    }
+
+    //Runtime: 25 ms, faster than 5.68% of Java online submissions for Two Out of Three.
+    //Memory Usage: 46.9 MB, less than 52.78% of Java online submissions for Two Out of Three.
+    static List<Integer> twoOutOfThree2(int[] nums1, int[] nums2, int[] nums3) {
+        List<Integer> list = new ArrayList<>(); //or use set
+        findInOtherArrays2(nums1, nums2, nums3, list);
+        findInOtherArrays2(nums2, nums1, nums3, list);
+        findInOtherArrays2(nums3, nums2, nums1, list);
+        return list.stream().distinct().collect(Collectors.toList());
+    }
+
+    private static void findInOtherArrays2(int[] nums1, int[] nums2, int[] nums3, List<Integer> list) {
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    list.add(nums1[i]);
+                }
+            }
+            for (int j = 0; j < nums3.length; j++) {
+                if (nums1[i] == nums3[j]) {
+                    list.add(nums1[i]);
+                }
+            }
+        }
+    }
+
     //Runtime: 19 ms, faster than 9.30% of Java online submissions for Two Out of Three.
     //Memory Usage: 49.1 MB, less than 5.31% of Java online submissions for Two Out of Three.
     static Map<Integer, Integer> map;
 
-    static List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+    static List<Integer> twoOutOfThree1(int[] nums1, int[] nums2, int[] nums3) {
         List<Integer> list = new ArrayList<>();
         map = new HashMap<>();
         putIntoMap(nums1);

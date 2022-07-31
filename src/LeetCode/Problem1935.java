11c27fa008e2adbef8ddb543c964d1830c3458f2
tbekpro <tbekpro@gmail.com>
2022-07-31 16:55:51 +0500

    https://leetcode.com/problems/maximum-number-of-words-you-can-type/ - easy level - optimized

--- src/LeetCode/Problem1935.java
+++ src/LeetCode/Problem1935.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,9 +13,29 @@ public class Problem1935 {
         System.out.println(canBeTypedWords("leet code", "e"));
     }
 
+    //Runtime: 4 ms, faster than 64.12% of Java online submissions for Maximum Number of Words You Can Type.
+    //Memory Usage: 42.3 MB, less than 82.82% of Java online submissions for Maximum Number of Words You Can Type.
+    static int canBeTypedWords(String text, String br) {
+        String[] words = text.split(" ");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            boolean contains = false;
+            for (int j = 0; j < br.length(); j++) {
+                if (words[i].contains(br.charAt(j) + "")) {
+                    contains = true;
+                    break;
+                }
+            }
+            if (!contains) {
+                list.add(words[i]);
+            }
+        }
+        return list.size();
+    }
+
     //Runtime: 10 ms, faster than 16.84% of Java online submissions for Maximum Number of Words You Can Type.
     //Memory Usage: 44.5 MB, less than 6.12% of Java online submissions for Maximum Number of Words You Can Type.
-    static int canBeTypedWords(String text, String br) {
+    static int canBeTypedWords3(String text, String br) {
         List<String> list = Arrays.asList(text.split(" "));
         for (int i = 0; i < br.length(); i++) {
             int finalI = i;

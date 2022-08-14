b9d97a446e7859805e6b185c0d19588ee17c157c
tbekpro <tbekpro@gmail.com>
2022-08-14 21:38:16 +0500

    https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/ - medium level

---
+++ src/LeetCode/medium/Problem921.java
@@ -0,0 +1,26 @@
+package LeetCode.medium;
+
+import java.util.Stack;
+
+public class Problem921 {
+    //https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+    public static void main(String[] args) {
+        System.out.println(minAddToMakeValid("())"));
+        System.out.println(minAddToMakeValid("((("));
+        System.out.println(minAddToMakeValid("((()()("));
+    }
+
+    //Runtime: 5 ms, faster than 5.80% of Java online submissions for Minimum Add to Make Parentheses Valid.
+    //Memory Usage: 41.8 MB, less than 74.08% of Java online submissions for Minimum Add to Make Parentheses Valid.
+    static int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.size() != 0 && stack.peek() == '(' && c == ')') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.size();
+    }
+}

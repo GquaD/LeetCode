b9caabff078ae7de9abcfa25af78fe74e37bd0ba
tbekpro <tbekpro@gmail.com>
2022-08-14 14:13:42 +0500

    https://leetcode.com/problems/design-a-stack-with-increment-operation/ - medium level

--- src/LeetCode/medium/Problem1381.java
+++ src/LeetCode/medium/Problem1381.java
@@ -20,14 +20,92 @@ public class Problem1381 {
         stack.pop();
         stack.pop();
     }
+    //improved performance
     //todo
+    //Runtime: 33 ms, faster than 16.75% of Java online submissions for Design a Stack With Increment Operation.
+    //Memory Usage: 50.9 MB, less than 28.29% of Java online submissions for Design a Stack With Increment Operation.
+    static class CustomStack {
+        int capacity;
+        BaseStack baseStack;
+
+        public CustomStack(int maxSize) {
+            this.capacity = maxSize;
+            this.baseStack = new BaseStack();
+        }
+
+        public void push(int x) {
+            if (this.baseStack.size < this.capacity) {
+                this.baseStack.push(x);
+            }
+        }
+
+        public int pop() {
+            return baseStack.pop();
+        }
+
+        public void increment(int k, int val) {
+            int times = Math.min(this.baseStack.size, k);
+            ListNode node = this.baseStack.top;
+            if (this.baseStack.size > k) {
+                int diff = this.baseStack.size - k;
+                for (int i = 0; i < diff; i++) {
+                    node = node.prev;
+                }
+            }
+            while (node != null) {
+                node.val += val;
+                node = node.prev;
+            }
+        }
+    }
+
+    static class BaseStack {
+        int size;
+        ListNode top;
+
+        public BaseStack() {
+            this.size = 0;
+            this.top = null;
+        }
+
+        public void push(int x) {
+            ListNode node = new ListNode(x);
+            node.prev = this.top;
+            this.top = node;
+            this.size++;
+        }
+
+        public int pop() {
+            int result = -1;
+            if (this.top == null) {
+                System.out.println("BaseStack size = 0");
+                return result;
+            } else {
+                result = this.top.val;
+                this.top = this.top.prev;
+                this.size--;
+            }
+            return result;
+        }
+    }
+
+    static class ListNode {
+        int val;
+        ListNode prev;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+
     //Runtime: 276 ms, faster than 5.06% of Java online submissions for Design a Stack With Increment Operation.
     //Memory Usage: 62 MB, less than 5.21% of Java online submissions for Design a Stack With Increment Operation.
-    static class CustomStack {
+    static class CustomStack1 {
         int capacity;
         List<Integer> list;
 
-        public CustomStack(int maxSize) {
+        public CustomStack1(int maxSize) {
             this.capacity = maxSize;
             this.list = new LinkedList<>();
         }

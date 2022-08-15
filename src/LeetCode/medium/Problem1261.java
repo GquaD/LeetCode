5a74d23f9aab79fe7a166fcdc1473893e52c909b
tbekpro <tbekpro@gmail.com>
2022-08-15 15:41:53 +0500

    https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/ - medium level

---
+++ src/LeetCode/medium/Problem1261.java
@@ -0,0 +1,130 @@
+package LeetCode.medium;
+
+import java.util.*;
+
+public class Problem1261 {
+    //https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/
+    public static void main(String[] args) {
+
+    }
+
+    //Runtime: 1584 ms, faster than 5.07% of Java online submissions for Find Elements in a Contaminated Binary Tree.
+    //Memory Usage: 52.5 MB, less than 73.25% of Java online submissions for Find Elements in a Contaminated Binary Tree.
+    class FindElements {
+        TreeNode root;
+        boolean exists;
+
+        public FindElements(TreeNode root) {
+            this.root = root;
+            recover(root, 0);
+        }
+
+        public boolean find(int target) {
+            this.exists = false;
+            find(root, target);
+            return this.exists;
+        }
+
+        private void find(TreeNode root, int target) {
+            if (root == null) {
+                return;
+            }
+            if (root.val == target) {
+                exists = true;
+                return;
+            }
+            find(root.left, target);
+            find(root.right, target);
+        }
+
+        private void recover(TreeNode root, int val) {
+            if (root == null) {
+                return;
+            }
+            root.val = val;
+            //left
+            recover(root.left, val * 2 + 1);
+            //right
+            recover(root.right, val * 2 + 2);
+        }
+    }
+
+    //Runtime: 480 ms, faster than 24.65% of Java online submissions for Find Elements in a Contaminated Binary Tree.
+    //Memory Usage: 47.1 MB, less than 93.71% of Java online submissions for Find Elements in a Contaminated Binary Tree
+    //if use linkedlist instead of arraylist
+
+    //Runtime: 48 ms, faster than 34.44% of Java online submissions for Find Elements in a Contaminated Binary Tree.
+    //Memory Usage: 57 MB, less than 21.33% of Java online submissions for Find Elements in a Contaminated Binary Tree.
+    //if use hashset instead of Arraylist
+    class FindElements2 {
+        Set<Integer> list;
+
+        public FindElements2(TreeNode root) {
+            list = new HashSet<>();
+            recover(root, 0);
+        }
+
+        public boolean find(int target) {
+            return list.contains(target);
+        }
+
+        private void recover(TreeNode root, int val) {
+            if (root == null) {
+                return;
+            }
+            root.val = val;
+            list.add(val);
+            //left
+            recover(root.left, val * 2 + 1);
+            //right
+            recover(root.right, val * 2 + 2);
+        }
+    }
+
+
+    //Runtime: 734 ms, faster than 20.46% of Java online submissions for Find Elements in a Contaminated Binary Tree.
+    //Memory Usage: 55.9 MB, less than 63.11% of Java online submissions for Find Elements in a Contaminated Binary Tree.
+    class FindElements1 {
+        List<Integer> list;
+
+        public FindElements1(TreeNode root) {
+            list = new LinkedList<>();
+            recover(root, 0);
+        }
+
+        public boolean find(int target) {
+            return list.contains(target);
+        }
+
+        private void recover(TreeNode root, int val) {
+            if (root == null) {
+                return;
+            }
+            root.val = val;
+            list.add(val);
+            //left
+            recover(root.left, val * 2 + 1);
+            //right
+            recover(root.right, val * 2 + 2);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

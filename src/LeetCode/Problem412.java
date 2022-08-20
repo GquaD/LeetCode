1a72da3ce5abd57ecab0ec3abf8fae0185b01af1
tbekpro <tbekpro@gmail.com>
2022-08-20 23:06:51 +0500

    https://leetcode.com/problems/fizz-buzz/ - easy level

---
+++ src/LeetCode/Problem412.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem412 {
+    //https://leetcode.com/problems/fizz-buzz/
+    public static void main(String[] args) {
+        LeetCodeUtils.printList(fizzBuzz(3));
+        LeetCodeUtils.printList(fizzBuzz(5));
+        LeetCodeUtils.printList(fizzBuzz(15));
+    }
+
+
+    //Runtime: 2 ms, faster than 75.27% of Java online submissions for Fizz Buzz.
+    //Memory Usage: 48.2 MB, less than 67.86% of Java online submissions for Fizz Buzz.
+    static List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add("" + i);
+            }
+        }
+        return list;
+    }
+}

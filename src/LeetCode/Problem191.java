3d889648311d02008d13672c97bbfe2b0bd14781
tbekpro <tbekpro@gmail.com>
2022-08-25 11:33:47 +0500

    https://leetcode.com/problems/number-of-1-bits/ - easy level

---
+++ src/LeetCode/Problem191.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class Problem191 {
+    //https://leetcode.com/problems/number-of-1-bits/
+    public static void main(String[] args) {
+
+    }
+
+    //Runtime: 1 ms, faster than 87.14% of Java online submissions for Number of 1 Bits.
+    //Memory Usage: 41.8 MB, less than 11.84% of Java online submissions for Number of 1 Bits.
+    static int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}

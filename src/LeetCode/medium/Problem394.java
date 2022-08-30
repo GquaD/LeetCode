2c5c9fffdf85299d65e792fd3f435521805e7578
tbekpro <tbekpro@gmail.com>
2022-08-30 23:47:21 +0500

    https://leetcode.com/problems/decode-string/ - medium level

--- src/LeetCode/medium/Problem394.java
+++ src/LeetCode/medium/Problem394.java
@@ -5,13 +5,71 @@ import java.util.Stack;
 public class Problem394 {
     //https://leetcode.com/problems/decode-string/
     public static void main(String[] args) {
-        System.out.println(decodeString("3[a]2[bc]"));
         System.out.println(decodeString("3[a2[c]]"));
+        System.out.println(decodeString("3[a]2[bc]"));
         System.out.println(decodeString("2[abc]3[cd]ef"));
         System.out.println(decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef")); //"zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef"
+        System.out.println("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkef"=="zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef");
     }
 
+
+    //Runtime: 2 ms, faster than 60.01% of Java online submissions for Decode String.
+    //Memory Usage: 42.7 MB, less than 17.17% of Java online submissions for Decode String.
     static String decodeString(String s) {
+        Stack<Character> stackBraces = new Stack<>();
+        Stack<Integer> stackMultipliers = new Stack<>();
+        Stack<String> stackString = new Stack<>();
+
+        StringBuilder sbResult = new StringBuilder();
+        StringBuilder string = new StringBuilder();
+        StringBuilder num = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            if (c - 48 <= 9) {
+                num.append(c);
+                continue;
+            }
+            if (!num.toString().equals("")) {
+                stackMultipliers.push(Integer.parseInt(num.toString()));
+                num.setLength(0);
+            }
+
+            if (c == '[') {
+                stackString.push(string.toString());
+                string.setLength(0);
+                stackBraces.push(c);
+                continue;
+            }
+
+            if (c == ']') {
+                stackBraces.pop();
+
+                String tempStr = stackString.pop();
+                int tempInt = stackMultipliers.pop();
+                StringBuilder tempSB = new StringBuilder();
+                for (int i = 0; i < tempInt; i++) {
+                    tempSB.append(string);
+                }
+                String tempStrForNow = tempStr + tempSB.toString();
+                if (string.toString().equals("")) {
+                    stackString.push(tempStrForNow);
+                }
+                string.setLength(0);
+                if (stackBraces.size() == 0) {
+                    sbResult.append(tempStrForNow);
+                } else {
+                    string.append(tempStrForNow);
+                }
+                continue;
+            }
+            string.append(c);
+        }
+        if (string.length() > 0) {
+            sbResult.append(string);
+        }
+        return sbResult.toString();
+    }
+
+    static String decodeString1(String s) {
         Stack<Integer> nums = new Stack<>();
         Stack<String> strings = new Stack<>();
         StringBuilder sbNum = new StringBuilder();

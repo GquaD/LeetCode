9054f4524c965d1eef0464ff67356abd617b62d1
gquad <tbekpro@gmail.com>
2022-09-12 21:02:26 +0500

    https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/ - medium --time exceeded

--- src/LeetCode/medium/Problem2275.java
+++ src/LeetCode/medium/Problem2275.java
@@ -6,36 +6,34 @@ import java.util.List;
 public class Problem2275 {
     //https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/
     public static void main(String[] args) {
-        System.out.println(largestCombination(new int[]{16,17,71,62,12,24,14}));
-        System.out.println(largestCombination(new int[]{8,8}));
+        System.out.println(largestCombination(new int[]{16, 17, 71, 62, 12, 24, 14}));
+        System.out.println(largestCombination(new int[]{8, 8}));
     }
 
     static List<List<Integer>> combinations;
     static int[] allNums;
+    static int maxSizeStatic;
+
     static int largestCombination(int[] candidates) {
         combinations = new ArrayList<>();
         allNums = candidates;
+        maxSizeStatic = 0;
         for (int i = 0; i < candidates.length; i++) {
             findCombinations(i, new ArrayList<>());
         }
-
-        int maxAndSize = 0;
-        for (int i = 0; i < combinations.size(); i++) {
-            List<Integer> combo = combinations.get(i);
-            int tempAnd = combo.get(0);
-            for (int j = 1; j < combo.size(); j++) {
-                tempAnd &= combo.get(j);
-            }
-            if (tempAnd > 0 && maxAndSize < combo.size()) {
-                maxAndSize = combo.size();
-            }
-        }
-        return maxAndSize;
+        return maxSizeStatic;
     }
 
     private static void findCombinations(int idxStart, List<Integer> nums) {
         if (idxStart >= allNums.length) {
-            combinations.add(new ArrayList<>(nums));
+            //combinations.add(new ArrayList<>(nums));
+            int tempAnd = nums.get(0);
+            for (int j = 1; j < nums.size(); j++) {
+                tempAnd &= nums.get(j);
+            }
+            if (tempAnd > 0 && maxSizeStatic < nums.size()) {
+                maxSizeStatic = nums.size();
+            }
             return;
         }
         nums.add(allNums[idxStart]);

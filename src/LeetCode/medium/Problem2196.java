566c4084269923d961451863cf4b374810933ec8
gquad <tbekpro@gmail.com>
2022-09-14 19:33:58 +0500

    https://leetcode.com/problems/create-binary-tree-from-descriptions/ - medium

--- src/LeetCode/medium/Problem2196.java
+++ src/LeetCode/medium/Problem2196.java
@@ -1,5 +1,9 @@
 package LeetCode.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Problem2196 {
     //https://leetcode.com/problems/create-binary-tree-from-descriptions/
     public static void main(String[] args) {
@@ -12,8 +16,53 @@ public class Problem2196 {
         });
     }
 
-    static TreeNode found;
+    //Runtime: 192 ms, faster than 14.01% of Java online submissions for Create Binary Tree From Descriptions.
+    //Memory Usage: 121.1 MB, less than 9.24% of Java online submissions for Create Binary Tree From Descriptions.
     static TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> map = new HashMap<>();
+        Map<Integer, Integer> parentMap = new HashMap<>();
+        for (int[] desc : descriptions) {
+            TreeNode curr = null;
+            int parent = desc[0], child = desc[1];
+            boolean isLeft = desc[2] == 1;
+            if (!map.containsKey(parent)) {
+                curr = new TreeNode(parent);
+                map.put(parent, curr);
+            }
+            curr = map.get(parent);
+            if (isLeft) {
+                TreeNode left = null;
+                if (!map.containsKey(child)) {
+                    left = new TreeNode(child);
+                    map.put(child, left);
+                }
+                left = map.get(child);
+                curr.left = map.get(child);
+                parentMap.put(left.val, curr.val);
+            } else {
+                TreeNode right = null;
+                if (!map.containsKey(child)) {
+                    right = new TreeNode(child);
+                    map.put(child, right);
+                }
+                right = map.get(child);
+                curr.right = map.get(child);
+                parentMap.put(right.val, curr.val);
+            }
+        }
+        TreeNode root = null;
+        for (Integer key : map.keySet()) {
+            if (!parentMap.containsKey(key)) {
+                root = map.get(key);
+                break;
+            }
+        }
+        return root;
+    }
+
+
+    static TreeNode found;
+    static TreeNode createBinaryTree1(int[][] descriptions) {
         found = null;
         int[] first = descriptions[0];
         TreeNode root = new TreeNode(first[0]);

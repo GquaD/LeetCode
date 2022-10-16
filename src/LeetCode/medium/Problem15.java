c2bbcdb799eef376aa97560f9eab6f4fed63b256
Behzod Tairov <tbekpro@gmail.com>
2022-10-16 17:40:20 +0500

    https://leetcode.com/problems/3sum/ - medium

---
+++ src/LeetCode/medium/Problem15.java
@@ -0,0 +1,286 @@
+package LeetCode.medium;
+
+import LeetCode.LeetCodeUtils;
+
+import java.util.*;
+
+public class Problem15 {
+    //https://leetcode.com/problems/3sum/
+    public static void main(String[] args) {
+        LeetCodeUtils.printListWithListsOfIntegers(threeSum(new int[]{-1,0,1,2,-1,-4}));
+        LeetCodeUtils.printListWithListsOfIntegers(threeSum(new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}));
+        int[] nums = new int[3000];
+        int start = -1500;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = start++;
+        }
+        //LeetCodeUtils.printListWithListsOfIntegers(threeSum(nums));
+    }
+
+    //https://leetcode.com/problems/3sum/solutions/2710157/java-o-n-log-n-n-n-log-n-solution/
+    //Runtime
+    //1618 ms
+    //Beats
+    //5%
+    //Memory
+    //148.8 MB
+    //Beats
+    //7.5%
+    static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); // n ^ 2
+        Set<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            int one = nums[i];
+            for (int j = i; j < nums.length; j++) {
+                if (i == j) continue;
+                int two = nums[j];
+                int three = 0 - one - two;
+                int idxThree = Arrays.binarySearch(nums, three);
+                if (idxThree >= 0 && idxThree != i && idxThree != j) {
+                    List<Integer> list = new ArrayList<>();
+                    if (three <= one) {
+                        list.add(three);
+                        list.add(one);
+                        list.add(two);
+                    } else if (three <= two) {
+                        list.add(one);
+                        list.add(three);
+                        list.add(two);
+                    } else {
+                        list.add(one);
+                        list.add(two);
+                        list.add(three);
+                    }
+                    //Collections.sort(list);
+                    result.add(list);
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    static List<List<Integer>> threeSum5(int[] nums) {
+        //num -> frequency
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        Set<Integer> set = map.keySet();
+        Set<List<Integer>> result = new HashSet<>();
+        for (int three : set) {
+            for (int i = 0; i < nums.length; i++) {
+                int one = nums[i];
+                int two = three + one;
+                if (set.contains(-two)) {
+
+                    List<Integer> list = new ArrayList<>();
+                    list.add(one);
+                    list.add(-two);
+                    list.add(three);
+                    Collections.sort(list);
+                    result.add(list);
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    //time limit exceeded 311/311
+    static List<List<Integer>> threeSum4(int[] nums) {
+        //num -> frequency
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        if (map.keySet().size() == 1 && map.keySet().contains(0)) {
+            List<Integer> list = new ArrayList<>();
+            list.add(0);
+            list.add(0);
+            list.add(0);
+            List<List<Integer>> res = new ArrayList<>();
+            res.add(list);
+            return res;
+        } else if (map.keySet().size() == 3 && map.keySet().contains(0)) {
+            int one = -1_000_000_000, two = 0;
+            List<List<Integer>> res = new ArrayList<>();
+            for (int num : map.keySet()) {
+                if (num < 0) one = num;
+                if (num > 0) two = num;
+            }
+            if (one + two == 0) {
+                List<Integer> list = new ArrayList<>();
+                list.add(one);
+                list.add(two);
+                list.add(0);
+                res.add(list);
+            }
+            if (map.get(0) >= 3) {
+                List<Integer> list = new ArrayList<>();
+                list.add(0);
+                list.add(0);
+                list.add(0);
+                res.add(list);
+            }
+            return res;
+        }
+
+        Set<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int one = nums[i];
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) continue;
+                int two = nums[j];
+                int sum = one + two;
+                int sumNeg = -sum;
+                List<Integer> list = new ArrayList<>();
+                list.add(one);
+                list.add(two);
+                list.add(sumNeg);
+                Collections.sort(list);
+                if (result.contains(list)) continue;
+                if (map.containsKey(sumNeg)) {
+                    if (one == two && two == sumNeg && map.get(sumNeg) >= 3) {
+                        result.add(list);
+                        map.put(one, map.get(one) - 1);
+                    } else if (one == two && one != sumNeg && map.get(one) >= 2) {
+                        result.add(list);
+                        map.put(one, map.get(one) - 1);
+                    }  else if (one == sumNeg && two != sumNeg && map.get(one) >= 2) {
+                        result.add(list);
+                        map.put(one, map.get(one) - 1);
+                    }  else if (two == sumNeg && one != sumNeg && map.get(two) >= 2) {
+                        result.add(list);
+                        map.put(two, map.get(two) - 1);
+                    } else if (one != two && two != sumNeg && one != sumNeg) {
+                        result.add(list);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    static List<List<Integer>> threeSum3(int[] nums) {
+        Arrays.sort(nums);
+        int min = nums[0], max = nums[nums.length - 1];
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                int sum = nums[i] + nums[j];
+                if (sum <= max && sum >= min) {
+                    for (int k = j; k < nums.length; k++) {
+                        if (sum + nums[k] == 0) {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[i]);
+                            list.add(nums[j]);
+                            list.add(nums[k]);
+                            Collections.sort(list);
+                            set.add(list);
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+    static List<List<Integer>> threeSum2(int[] nums) {
+        Arrays.sort(nums);
+        //sum -> set of indices
+        Map<Integer, Set<List<Integer>>> map = new HashMap<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1, k = 0; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                List<Integer> pair = new ArrayList<>();
+                pair.add(i);
+                pair.add(j);
+                pair.add(k);
+                if (!map.containsKey(sum)) {
+                    Set<List<Integer>> set = new HashSet<>();
+                    set.add(pair);
+                    map.put(sum, set);
+                } else {
+                    map.get(sum).add(pair);
+                }
+            }
+        }
+        Set<List<Integer>> res = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Set<List<Integer>> set = map.get(-nums[i]);
+            if (set != null) {
+                for (List<Integer> pair : set){
+                    int one = pair.get(0), two = pair.get(1);
+                    if (i != one && i != two && one != two) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[one]);
+                        list.add(nums[two]);
+                        list.add(nums[i]);
+                        Collections.sort(list);
+                        res.add(list);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(res);
+    }
+
+    //time limit exceeded
+    //307 done out of 311 tests
+    static List<List<Integer>> threeSum1(int[] nums) {
+        Arrays.sort(nums);
+        //sum -> set of indices
+        Map<Integer, Set<List<Integer>>> map = new HashMap<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                List<Integer> pair = new ArrayList<>();
+                pair.add(i);
+                pair.add(j);
+                if (!map.containsKey(sum)) {
+                    Set<List<Integer>> set = new HashSet<>();
+                    set.add(pair);
+                    map.put(sum, set);
+                } else {
+                    map.get(sum).add(pair);
+                }
+            }
+        }
+        Set<List<Integer>> res = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Set<List<Integer>> set = map.get(-nums[i]);
+            if (set != null) {
+                for (List<Integer> pair : set){
+                    int one = pair.get(0), two = pair.get(1);
+                    if (i != one && i != two && one != two) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[one]);
+                        list.add(nums[two]);
+                        list.add(nums[i]);
+                        Collections.sort(list);
+                        res.add(list);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(res);
+    }
+
+    static class Pair {
+        int one;
+        int two;
+
+        public Pair(int one, int two) {
+            this.one = one;
+            this.two = two;
+        }
+    }
+}

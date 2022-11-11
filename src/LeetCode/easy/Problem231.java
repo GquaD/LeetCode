3fb2abf0c2e8da42837376cc04f509c04b68848f
Behzod Tairov <tbekpro@gmail.com>
2022-11-11 12:49:27 +0500

    https://leetcode.com/problems/power-of-two/ - easy, solution 3

--- src/LeetCode/easy/Problem231.java
+++ src/LeetCode/easy/Problem231.java
@@ -14,7 +14,39 @@ public class Problem231 {
         System.out.println(isPowerOfTwo(4096));
     }
 
+    //https://leetcode.com/problems/power-of-two/solutions/2802701/java-7-solutions-oh-my-gosh/
+    //https://leetcode.com/problems/power-of-two/solutions/2802677/java-3-lines-solution/
+    //Runtime
+    //3 ms
+    //Beats
+    //14.6%
+    //Memory
+    //41.8 MB
+    //Beats
+    //16.24%
+    static boolean isPowerOfTwo(int n) {
+        if (n < 1) return false;
+        if (Integer.toBinaryString(n).substring(1).contains("1")) return false;
+        return true;
+    }
 
+    //Runtime
+    //3 ms
+    //Beats
+    //14.6%
+    //Memory
+    //41.8 MB
+    //Beats
+    //16.24%
+    static boolean isPowerOfTwo4(int n) {
+        if (n < 1) return false;
+        char[] arr = Integer.toBinaryString(n).toCharArray();
+        if (arr[0] != '1') return false;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == '1') return false;
+        }
+        return true;
+    }
 
 
     //Runtime
@@ -26,7 +58,7 @@ public class Problem231 {
     //Beats
     //37.12%
     static int[] vals = new int[31];
-    static boolean isPowerOfTwo(int n) {
+    static boolean isPowerOfTwo3(int n) {
         if (n < 1) return false;
         if (vals[0] == 0) {
             fillVals();

8dbd8a889e4215132b412156027cd450b094561e
Behzod Tairov <tbekpro@gmail.com>
2022-11-19 12:21:28 +0500

    https://leetcode.com/problems/increasing-order-search-tree/ - easy

--- src/LeetCode/easy/Problem897.java
+++ src/LeetCode/easy/Problem897.java
@@ -1,5 +1,8 @@
 package LeetCode.easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem897 {
     //https://leetcode.com/problems/increasing-order-search-tree/
     public static void main(String[] args) {
@@ -10,27 +13,33 @@ public class Problem897 {
         n5.right = n7;
         increasingBST(n5);
     }
-    static TreeNode head;
-    static TreeNode current;
+
+    //30 min
+    //Runtime
+    //1 ms
+    //Beats
+    //40.76%
+    //Memory
+    //42.5 MB
+    //Beats
+    //7.51%
+    static List<Integer> list;
     static TreeNode increasingBST(TreeNode root) {
-        current = null;
-        head = null;
+        list = new ArrayList<>();
         traverseBSTInorder(root);
+        TreeNode head = new TreeNode(list.get(0)), temp = head;
+        for (int i = 1; i < list.size(); i++) {
+            temp.right = new TreeNode(list.get(i));
+            temp = temp.right;
+        }
         return head;
     }
 
     private static void traverseBSTInorder(TreeNode node) {
         if (node == null) return;
 
-        if (current != null) {
-            current.right = new TreeNode(node.val);
-        }
-
         traverseBSTInorder(node.left);
+        list.add(node.val);
         traverseBSTInorder(node.right);
-        if (head == null) {
-            head = new TreeNode(node.val);
-            current = head;
-        }
     }
 }

918da9e5f528acf393b83b041c1ac42cadd7088c
Behzod Tairov <tbekpro@gmail.com>
2022-11-24 12:09:29 +0500

    https://leetcode.com/problems/reverse-bits/description/ - easy

---
+++ src/LeetCode/easy/Problem190.java
@@ -0,0 +1,21 @@
+package LeetCode.easy;
+
+public class Problem190 {
+    //https://leetcode.com/problems/reverse-bits/description/
+    public static void main(String[] args) {
+
+    }
+
+    //https://leetcode.com/problems/reverse-bits/solutions/2844307/java-1-line-1ms-solution/
+    //Runtime
+    //1 ms
+    //Beats
+    //97.92%
+    //Memory
+    //42.8 MB
+    //Beats
+    //18.82%
+    public int reverseBits1(int n) {
+        return Integer.reverse(n);
+    }
+}

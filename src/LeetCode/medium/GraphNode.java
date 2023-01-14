8f431678f34819cbc18d756a3fd974b4af6238f3
Behzod Tairov <tbekpro@gmail.com>
2023-01-14 10:58:51 +0500

    helper classes

---
+++ src/LeetCode/medium/GraphNode.java
@@ -0,0 +1,21 @@
+package LeetCode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphNode {
+    public int val;
+    public List<GraphNode> neighbors;
+    public GraphNode() {
+        val = 0;
+        neighbors = new ArrayList<GraphNode>();
+    }
+    public GraphNode(int _val) {
+        val = _val;
+        neighbors = new ArrayList<GraphNode>();
+    }
+    public GraphNode(int _val, ArrayList<GraphNode> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
--- src/LeetCode/medium/NTreeNode.java
+++ src/LeetCode/medium/NTreeNode.java
@@ -1,5 +1,6 @@
 package LeetCode.medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NTreeNode {
@@ -10,6 +11,7 @@ public class NTreeNode {
 
     public NTreeNode(int _val) {
         val = _val;
+        children = new ArrayList<>();
     }
 
     public NTreeNode(int _val, List<NTreeNode> _children) {

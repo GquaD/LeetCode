7c26f9b7f56c71ed803212ac38593960ce8398d0
Behzod Tairov <tbekpro@gmail.com>
2023-02-24 11:25:26 +0500

    https://leetcode.com/problems/ugly-number-ii/ - medium

--- src/LeetCode/medium/Problem264.java
+++ src/LeetCode/medium/Problem264.java
@@ -1,9 +1,6 @@
 package LeetCode.medium;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Problem264 {
     //https://leetcode.com/problems/ugly-number-ii/
@@ -19,16 +16,16 @@ public class Problem264 {
         System.out.println(nthUglyNumber(500));
     }
 
-    //
+    //https://leetcode.com/problems/ugly-number-ii/solutions/3224147/java-2ms-99-faster-cached-solution/
     //40 - 50 min
     //Runtime
-    //3 ms
+    //2 ms
     //Beats
-    //67.24%
+    //98.64%
     //Memory
-    //42.1 MB
+    //42.2 MB
     //Beats
-    //33.40%
+    //23.77%
     static int[] ugly;
 
     static int nthUglyNumber(int n) {
@@ -41,31 +38,32 @@ public class Problem264 {
         fillArrayWith(2, arr2);
         fillArrayWith(3, arr3);
         fillArrayWith(5, arr5);
-        TreeSet<Integer> pq = new TreeSet<>();
+        Set<Integer> set = new HashSet<>();
         long temp = 1;
         for (int i = 0; i < arr2.length; i++) {
             temp *= arr2[i];
             if (temp < Integer.MAX_VALUE) {
-                pq.add((int)temp);
+                set.add((int)temp);
             } else break;
             long temp3 = temp;
             for (int j = 0; j < arr3.length; j++) {
                 temp3 *= arr3[j];
                 if (temp3 < Integer.MAX_VALUE) {
-                    pq.add((int)temp3);
+                    set.add((int)temp3);
                 } else break;
                 long temp5 = temp3;
                 for (int k = 0; k < arr5.length; k++) {
                     temp5 *= arr5[k];
                     if (temp5 < Integer.MAX_VALUE) {
-                        pq.add((int)temp5);
+                        set.add((int)temp5);
                     } else break;;
                 }
             }
         }
-        int[] res = new int[pq.size()];
+        int[] res = new int[set.size()];
         int count = 0;
-        while (!pq.isEmpty()) res[count++] = pq.pollFirst();
+        for (int n : set) res[count++] = n;
+        Arrays.sort(res);
         return res;
     }
 

857e9352b712066ea098ad636a0ca103fc01e056
Behzod Tairov <tbekpro@gmail.com>
2023-02-26 13:04:30 +0500

    https://leetcode.com/problems/h-index-ii/ - medium

--- src/LeetCode/medium/Problem275.java
+++ src/LeetCode/medium/Problem275.java
@@ -10,16 +10,25 @@ public class Problem275 {
         System.out.println(hIndex(new int[]{1,2,3,4,5,6,7,8}) + " vs 4");
     }
 
+    //nnnn
+    //O(logN)
+    //Runtime
+    //0 ms
+    //Beats
+    //100%
+    //Memory
+    //49.9 MB
+    //Beats
+    //18.86%
     static int hIndex(int[] c) {
-        int h = c.length;
-        int left = 0, right = c.length - 1, mid = 0;
+        int left = 0, right = c.length - 1;
         while (left <= right) {
-            mid = (left + right) / 2;
-            if (mid >= c[mid]) right = mid - 1;
-            else left = mid + 1;
+            int mid = (left + right) / 2, diff = c.length - mid;
+            if (c[mid] == diff) return diff;
+            else if (c[mid] < diff) left = mid + 1;
+            else right = mid - 1;
         }
-
-        return mid;
+        return c.length - left;
     }
 
     //not O(logN)

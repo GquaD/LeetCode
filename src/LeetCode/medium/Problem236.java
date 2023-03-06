17449b9c8d89d565d710a9e1b852df336ab48148
Behzod Tairov <tbekpro@gmail.com>
2023-03-06 11:04:13 +0500

    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/ - medium

---
+++ src/LeetCode/medium/Problem236.java
@@ -0,0 +1,46 @@
+package LeetCode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem236 {
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+    public static void main(String[] args) {
+
+    }
+
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solutions/3262947/java-dfs-solution/
+    //15 min
+    //Runtime
+    //8 ms
+    //Beats
+    //25.47%
+    //Memory
+    //44.5 MB
+    //Beats
+    //18.70%
+    static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<List<TreeNode>> listBig = new ArrayList<>();
+        goDFS(root, p, q, new ArrayList<>(), listBig);
+        List<TreeNode> one = listBig.get(0), two = listBig.get(1);
+        for (int i = one.size() - 1; i >= 0; i--) {
+            TreeNode node1 = one.get(i);
+            for (int j = two.size() - 1; j >= 0; j--) {
+                TreeNode node2 = two.get(j);
+                if (node1.equals(node2)) return node1;
+            }
+        }
+        return root;
+    }
+
+    private static void goDFS(TreeNode node, TreeNode p, TreeNode q, List<TreeNode> list, List<List<TreeNode>> listBig) {
+        if (node == null) return;
+
+        list.add(node);
+        if (node.val == p.val || node.val == q.val) listBig.add(new ArrayList<>(list));
+
+        goDFS(node.left, p, q, list, listBig);
+        goDFS(node.right, p, q, list, listBig);
+        list.remove(list.size() - 1);
+    }
+}

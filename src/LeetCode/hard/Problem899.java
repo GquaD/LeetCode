887761a0644da6514c238724137accd637f742c5
Behzod Tairov <tbekpro@gmail.com>
2023-03-20 10:09:28 +0500

    https://leetcode.com/problems/orderly-queue/ - hard

--- src/LeetCode/hard/Problem899.java
+++ src/LeetCode/hard/Problem899.java
@@ -1,14 +1,52 @@
 package LeetCode.hard;
 
+import java.util.PriorityQueue;
+
 public class Problem899 {
     //https://leetcode.com/problems/orderly-queue/
     public static void main(String[] args) {
 
     }
 
-    //todo
-    //todo
+    //https://leetcode.com/problems/orderly-queue/solutions/3318494/java-using-priorityqueue/
+    //15 min
+    //Runtime
+    //6 ms
+    //Beats
+    //31.73%
+    //Memory
+    //43.2 MB
+    //Beats
+    //21.15%
     static String orderlyQueue(String s, int k) {
+        if (k > 1) {
+            PriorityQueue<Character> pq = new PriorityQueue<>();
+            for (int i = 0; i < s.length(); i++) {
+                pq.offer(s.charAt(i));
+            }
+            StringBuilder sb = new StringBuilder();
+            while (!pq.isEmpty()) {
+                sb.append(pq.poll());
+            }
+            return sb.toString();
+        } else {
+            StringBuilder sb = new StringBuilder();
+            PriorityQueue<String> pq = new PriorityQueue<>();
+            pq.offer(s);
+            String temp = s;
+            for (int i = 0; i < s.length(); i++) {
+                sb.append(temp, 1, temp.length()).append(temp, 0, 1);
+                temp = sb.toString();
+                pq.offer(temp);
+                sb.setLength(0);
+            }
+            return pq.poll();
+        }
+    }
+
+    //todo
+    //todo
+    static String orderlyQueue1(String s, int k) {
         if (k == 1) {
             char c = 'z'; //smallest char
             for (int i = 0; i < s.length(); i++) {

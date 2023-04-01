295143cb24afee19ed5373e7d1e9c07ffbad82e5
Behzod Tairov <tbekpro@gmail.com>
2023-04-01 08:22:44 +0500

    https://leetcode.com/problems/binary-search/ - easy

--- src/LeetCode/easy/Problem704.java
+++ src/LeetCode/easy/Problem704.java
@@ -11,9 +11,22 @@ public class Problem704 {
         System.out.println(search(new int[]{-1, 0,3,5,9,23}, 9));
         System.out.println(search(new int[]{-1, 0,3,5,9,23}, 2));
     }
+
+    static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2, curr = nums[mid];
+            if (curr == target) return mid;
+            else if (curr < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+
+
     //Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Search.
     //Memory Usage: 54.7 MB, less than 20.40% of Java online submissions for Binary Search.
-    static int search(int[] nums, int target) {
+    static int search1(int[] nums, int target) {
         int result = Arrays.binarySearch(nums, target);
         return result < 0 ? -1 : result;
     }

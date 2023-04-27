24931c0af7913a5258521ba56aa7088ebd252e65
Behzod Tairov <tbekpro@gmail.com>
2023-04-27 09:32:37 +0500

    https://leetcode.com/problems/bulb-switcher/description/ - medium

---
+++ src/LeetCode/medium/Problem319.java
@@ -0,0 +1,21 @@
+package LeetCode.medium;
+
+public class Problem319 {
+    //https://leetcode.com/problems/bulb-switcher/description/
+    public static void main(String[] args) {
+
+    }
+
+    //10 min | solved on paper
+    //Runtime
+    //0 ms
+    //Beats
+    //100%
+    //Memory
+    //39.3 MB
+    //Beats
+    //68.86%
+    static int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}

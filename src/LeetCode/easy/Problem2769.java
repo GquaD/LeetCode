994b7a7a409c4ee0f1b6e276b4eb3d0289ab31b5
Behzod Tairov <tbekpro@gmail.com>
2023-07-09 14:24:12 -0400

    https://leetcode.com/problems/find-the-maximum-achievable-number/ - easy

---
+++ src/LeetCode/easy/Problem2769.java
@@ -0,0 +1,22 @@
+package LeetCode.easy;
+
+public class Problem2769 {
+    //https://leetcode.com/problems/find-the-maximum-achievable-number/
+    public static void main(String[] args) {
+
+    }
+
+    //1 min
+    //Runtime
+    //2 ms
+    //Beats
+    //100%
+    //Sorry, there are not enough accepted submissions to show data
+    //Memory
+    //40.3 MB
+    //Beats
+    //100%
+    static int theMaximumAchievableX(int num, int t) {
+        return num + (t * 2);
+    }
+}

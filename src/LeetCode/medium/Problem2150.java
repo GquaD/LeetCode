845c71b3860cd1d3aa5d07d6f6a28bf1e9d11142
Behzod Tairov <tbekpro@gmail.com>
2023-09-24 12:38:32 -0400

    https://leetcode.com/problems/find-all-lonely-numbers-in-the-array/ - medium

--- src/LeetCode/medium/Problem2150.java
+++ src/LeetCode/medium/Problem2150.java
@@ -8,6 +8,26 @@ public class Problem2150 {
 
     }
 
+    //https://leetcode.com/problems/find-all-lonely-numbers-in-the-array/solutions/4085503/java-from-80ms-to-31ms/
+    //5-10min
+    //Runtime
+    //31 ms
+    //Beats
+    //80.73%
+    //Memory
+    //59.3 MB
+    //Beats
+    //90.65%
+    static List<Integer> findLonely(int[] nums) {
+        if (nums.length == 1) return Arrays.asList(nums[0]);
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < nums.length - 1; i++) if (nums[i] - nums[i - 1] > 1 && nums[i + 1] - nums[i] > 1) list.add(nums[i]);
+        if (nums[1] - nums[0] > 1) list.add(nums[0]);
+        if (nums[nums.length - 1] - nums[nums.length - 2] > 1) list.add(nums[nums.length - 1]);
+        return list;
+    }
+
     //2min
     //Runtime
     //80 ms

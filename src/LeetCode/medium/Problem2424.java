21908cf20f6f52ecedc37431bd1de3e7c14dae79
Behzod Tairov <tbekpro@gmail.com>
2023-11-25 09:23:32 -0500

    https://leetcode.com/problems/longest-uploaded-prefix/ - medium

--- src/LeetCode/medium/Problem2424.java
+++ src/LeetCode/medium/Problem2424.java
@@ -3,20 +3,42 @@ package LeetCode.medium;
 public class Problem2424 {
     //https://leetcode.com/problems/longest-uploaded-prefix/
     public static void main(String[] args) {
-        LUPrefix luPrefix = new LUPrefix(4);
+        /*LUPrefix luPrefix = new LUPrefix(4);
         luPrefix.upload(3);
         System.out.println(luPrefix.longest());
         luPrefix.upload(1);
         System.out.println(luPrefix.longest());
         luPrefix.upload(2);
+        System.out.println(luPrefix.longest());*/
+
+        LUPrefix luPrefix = new LUPrefix(10);
+        System.out.println(luPrefix.longest());
+        luPrefix.upload(6);
+        System.out.println(luPrefix.longest());
+        luPrefix.upload(10);
+        luPrefix.upload(7);
+        luPrefix.upload(4);
+        System.out.println(luPrefix.longest());
+        luPrefix.upload(2);
+        System.out.println(luPrefix.longest());
+        luPrefix.upload(8);
+        luPrefix.upload(3);
+        luPrefix.upload(1);
         System.out.println(luPrefix.longest());
     }
 
 
 }
-
-//Wrong Answer
-//18 / 24 testcases passed
+//https://leetcode.com/problems/longest-uploaded-prefix/solutions/4328501/java-array-max-value/
+//15min
+//Runtime
+//26 ms
+//Beats
+//86.92%
+//Memory
+//106.6 MB
+//Beats
+//83.8%
 class LUPrefix {
 
     int res;
@@ -38,6 +60,7 @@ class LUPrefix {
         boolean exists = true;
         for (int i = res; i <= max; i++) {
             if (arr[i] == 0) {
+                res = Math.max(i - 1, res);
                 exists = false;
                 break;
             }

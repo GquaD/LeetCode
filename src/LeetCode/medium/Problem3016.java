3e572103e83f1ff0213644197bd55749042691ea
Behzod Tairov <tbekpro@gmail.com>
2024-01-22 08:20:05 -0500

    https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/ - medium - new faster solution

--- src/LeetCode/medium/Problem3016.java
+++ src/LeetCode/medium/Problem3016.java
@@ -7,6 +7,29 @@ public class Problem3016 {
 
     }
 
+    //https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/solutions/4609045/java-o-n-100-faster/
+    //Runtime
+    //8
+    //ms
+    //Beats
+    //100.00%
+    //of users with Java
+    //Memory
+    //45.27
+    //MB
+    //Beats
+    //100.00%
+    //of users with Java
+
+    static int minimumPushesq(String w) {
+        int[] alph = new int[26];
+        for (int i = 0; i < w.length(); i++) alph[w.charAt(i) - 'a']++;
+        Arrays.sort(alph);
+        int sum = 0, count = 8;
+        for (int i = 25; i >= 0; i--) sum += alph[i] * (count++ / 8);
+        return sum;
+    }
+
     //https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/solutions/4609014/java-100-faster-hashmap-priorityqueue/
     //10-15 min
     //Runtime
@@ -21,7 +44,7 @@ public class Problem3016 {
     //Beats
     //100.00%
     //of users with Java
-    static int minimumPushes(String word) {
+    static int minimumPushesq1(String word) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < word.length(); i++) {
             map.put(word.charAt(i), map.getOrDefault(word.charAt(i), 0) + 1);

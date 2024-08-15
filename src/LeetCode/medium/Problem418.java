0a5f77bcd87b82b6db0914722351502f312e936f
Behzod Tairov <tbekpro@gmail.com>
2024-08-15 08:16:55 -0400

    https://leetcode.com/problems/sentence-screen-fitting/ - medium

--- src/LeetCode/medium/Problem418.java
+++ src/LeetCode/medium/Problem418.java
@@ -6,20 +6,17 @@ public class Problem418 {
 
     //1h nnn
     //Runtime
-    //2729
+    //2656
     //ms
     //Beats
     //5.09%
     //Analyze Complexity
     //Memory
-    //40.83
+    //40.92
     //MB
     //Beats
-    //63.53%
+    //47.45%
     public int wordsTyping(String[] s, int rows, int cols) {
-        for (String str : s) {
-            if (str.length() > cols) return 0;
-        }
         int words = 0, r = 1, c = cols, count = 0;
         while (r <= rows) {
             int len = s[words % s.length].length();

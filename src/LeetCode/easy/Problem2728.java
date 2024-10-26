db96b139bf980d91b43434610255bb855af4a60e
Behzod Tairov <tbekpro@gmail.com>
2024-10-26 07:07:05 -0400

    https://leetcode.com/problems/count-houses-in-a-circular-street - easy

---
+++ src/LeetCode/easy/Problem2728.java
@@ -0,0 +1,36 @@
+package LeetCode.easy;
+
+public class Problem2728 {
+    //https://leetcode.com/problems/count-houses-in-a-circular-street
+    public static void main(String[] args) {
+
+    }
+
+    //2min
+    //n
+    //Runtime
+    //2
+    //ms
+    //Beats
+    //85.71%
+    //Analyze Complexity
+    //Memory
+    //44.57
+    //MB
+    //Beats
+    //28.57%
+//    public int houseCount(Street street, int k) {
+//        for (int i = 0; i < k; i++) {
+//            street.closeDoor();
+//            street.moveRight();
+//        }
+//        street.openDoor();
+//        street.moveRight();
+//        int count = 1;
+//        while (!street.isDoorOpen()) {
+//            count++;
+//            street.moveRight();
+//        }
+//        return count;
+//    }
+}

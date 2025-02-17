443194f145476d294fab3fdf7cc1b3354236ca1b
Behzod Tairov <tbekpro@gmail.com>
2025-02-17 12:14:43 -0500

    https://leetcode.com/problems/add-two-numbers/ - medium

--- src/LeetCode/medium/Problem2.java
+++ src/LeetCode/medium/Problem2.java
@@ -5,11 +5,50 @@ public class Problem2 {
     public static void main(String[] args) {
 
     }
+
+    //https://leetcode.com/problems/add-two-numbers/solutions/6434234/java-1ms-100-faster-solution-by-tbekpro-th1b/
+    //10min
+    //Runtime
+    //1
+    //ms
+    //Beats
+    //100.00%
+    //Analyze Complexity
+    //Memory
+    //44.59
+    //MB
+    //Beats
+    //51.97%
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0), prev = head;
+
+        int carry = 0;
+        while (!(l1 == null && l2 == null)) {
+            int val1 = l1 == null ? 0 : l1.val, val2 = l2 == null ? 0 : l2.val;
+            int cur = val1 + val2 + carry;
+            prev.val = cur % 10;
+            carry = cur / 10;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+
+            if (!(l1 == null && l2 == null)) {
+                prev.next = new ListNode(0);
+                prev = prev.next;
+            }
+        }
+        if (carry > 0) prev.next = new ListNode(carry);
+        return head;
+    }
+
+
     //https://leetcode.com/problems/add-two-numbers/discuss/2596686/Java-Messy-solution-or-84-faster
     //30 mins
     //Runtime: 3 ms, faster than 81.14% of Java online submissions for Add Two Numbers.
     //Memory Usage: 47.7 MB, less than 56.04% of Java online submissions for Add Two Numbers.
-    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    static ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
         ListNode node1 = l1;
         ListNode node2 = l2;
         ListNode last;

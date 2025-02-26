f50701c818a847822b7723743a591b059897a03e
Behzod Tairov <tbekpro@gmail.com>
2025-02-26 09:26:53 -0500

    https://leetcode.com/problems/simplify-path/discussion/ - medium

--- src/LeetCode/medium/Problem71.java
+++ src/LeetCode/medium/Problem71.java
@@ -17,6 +17,39 @@ public class Problem71 {
         System.out.println(simplifyPath("/a//b//c//////d"));
     }
 
+    //5min
+    //Runtime
+    //3
+    //ms
+    //Beats
+    //98.66%
+    //Analyze Complexity
+    //Memory
+    //43.21
+    //MB
+    //Beats
+    //83.71%
+    public String simplifyPath3(String path) {
+        String[] split = path.split("/");
+        List<String> folders = new ArrayList<>();
+        for (String s: split) {
+            if (s.length() > 0) {
+                if (s.equals("..")) {
+                    if (folders.size() > 0)
+                        folders.remove(folders.size() - 1);
+                } else if (!s.equals(".")) {
+                    folders.add(s);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String folder : folders) {
+            sb.append("/");
+            sb.append(folder);
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+
     //https://leetcode.com/problems/simplify-path/solutions/3187875/java-99-time-99-memory-solution/
     //15 min
     //Runtime

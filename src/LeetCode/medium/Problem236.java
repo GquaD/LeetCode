a55ceb5e6045b011038d76965df7a6702f99cb93
Behzod Tairov <tbekpro@gmail.com>
2025-02-26 21:49:36 -0500

    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/ - medium

--- src/LeetCode/medium/Problem236.java
+++ src/LeetCode/medium/Problem236.java
@@ -9,6 +9,38 @@ public class Problem236 {
 
     }
 
+    //10min
+    //Runtime
+    //6
+    //ms
+    //Beats
+    //100.00%
+    //Analyze Complexity
+    //Memory
+    //44.15
+    //MB
+    //Beats
+    //99.38%
+    //public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //        TreeNode[] result = new TreeNode[1];
+    //        goDFS(root, p.val, q.val, result);
+    //        return result[0];
+    //    }
+    //
+    //    private int goDFS(TreeNode node, int p, int q, TreeNode[] result) {
+    //        if (node == null) return 0;
+    //
+    //        int left = goDFS(node.left, p, q, result), right = goDFS(node.right, p, q, result);
+    //        if (node.val == p || node.val == q) {
+    //            left++;
+    //        }
+    //        left += right;
+    //        if (left >= 2 && result[0] == null) {
+    //            result[0] = node;
+    //        }
+    //        return left;
+    //    }
+
     //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solutions/3262947/java-dfs-solution/
     //15 min
     //Runtime

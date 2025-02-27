33d4124843c45568a542c2502ac537f691f74b23
Behzod Tairov <tbekpro@gmail.com>
2025-02-27 10:28:28 -0500

    https://leetcode.com/problems/binary-tree-vertical-order-traversal/ - medium

--- src/LeetCode/medium/Problem314.java
+++ src/LeetCode/medium/Problem314.java
@@ -10,6 +10,60 @@ public class Problem314 {
 
     }
 
+    //15min
+    //Runtime
+    //4
+    //ms
+    //Beats
+    //23.49%
+    //Analyze Complexity
+    //Memory
+    //43.38
+    //MB
+    //Beats
+    //6.46%
+    //public List<List<Integer>> verticalOrder(TreeNode root) {
+    //        Map<Integer, List<Pair>> map = new TreeMap<>();
+    //
+    //        goDFS(root, map, 0, 0);
+    //
+    //        List<List<Integer>> result = new ArrayList<>();
+    //        for (int column: map.keySet()) {
+    //            List<Integer> list = new ArrayList<>();
+    //            List<Pair> l = map.get(column);
+    //            Collections.sort(l, (a, b) -> a.lvl - b.lvl);
+    //            for (Pair p: l) {
+    //                list.add(p.val);
+    //            }
+    //            result.add(list);
+    //        }
+    //        return result;
+    //    }
+    //
+    //    private void goDFS(TreeNode node, Map<Integer, List<Pair>> map, int column, int level) {
+    //        if (node == null) return;
+    //
+    //        List<Pair> list = map.get(column);
+    //        if (list == null) {
+    //            list = new ArrayList<>();
+    //            map.put(column, list);
+    //        }
+    //        list.add(new Pair(column, level, node.val));
+    //
+    //        goDFS(node.left, map, column - 1, level + 1);
+    //        goDFS(node.right, map, column + 1, level + 1);
+    //    }
+    //
+    //    class Pair {
+    //        int col, lvl, val;
+    //
+    //        public Pair (int c, int l, int v) {
+    //            col = c;
+    //            lvl = l;
+    //            val = v;
+    //        }
+    //    }
+
     //https://leetcode.com/problems/binary-tree-vertical-order-traversal/solutions/5188492/java-bfs-treemap-solution/
     //20min
     //Runtime

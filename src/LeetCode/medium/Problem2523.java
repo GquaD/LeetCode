a51e9af75100e20abc01e8a86d51b176698966e8
Behzod Tairov <tbekpro@gmail.com>
2025-03-07 16:57:20 -0500

    https://leetcode.com/problems/closest-prime-numbers-in-range - medium

--- src/LeetCode/medium/Problem2523.java
+++ src/LeetCode/medium/Problem2523.java
@@ -9,6 +9,59 @@ public class Problem2523 {
 
     }
 
+
+    //2min
+    //Runtime
+    //336
+    //ms
+    //Beats
+    //15.87%
+    //Analyze Complexity
+    //Memory
+    //56.26
+    //MB
+    //Beats
+    //10.32%
+    //class Solution {
+    //
+    //    private static Set<Integer> primes;
+    //
+    //    public int[] closestPrimes(int left, int right) {
+    //        if (primes == null) {
+    //            primes = new HashSet<>();
+    //            for (int i = 2; i <= 1_000_000; i++) {
+    //                if (isPrime(i)) primes.add(i);
+    //            }
+    //        }
+    //
+    //        List<Integer> list = new ArrayList<>();
+    //        for (int i = left; i <= right; i++) {
+    //            if (primes.contains(i)) {
+    //                list.add(i);
+    //            }
+    //        }
+    //        if (list.size() < 2) return new int[]{-1, -1};
+    //        int min = Integer.MAX_VALUE, res[] = new int[]{-1, -1};
+    //        for (int i = 1; i < list.size(); i++) {
+    //            int a = list.get(i - 1), b = list.get(i);
+    //            if (b - a < min) {
+    //                min = b - a;
+    //                res[0] = a;
+    //                res[1] = b;
+    //            }
+    //        }
+    //        return res;
+    //    }
+    //
+    //    private boolean isPrime(int n) {
+    //        if (n == 1) return false;
+    //        for (int i = 2; i <= Math.sqrt(n); i++) {
+    //            if (n % i == 0) return false;
+    //        }
+    //        return true;
+    //    }
+    //}
+
     //https://leetcode.com/problems/closest-prime-numbers-in-range/solutions/6509267/java-straightforward-solution/
     //10min
     //Runtime

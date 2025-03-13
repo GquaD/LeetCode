d10f0591ac75c7dcce3cc48a2613223c084b9c5a
Behzod Tairov <tbekpro@gmail.com>
2025-03-13 08:23:06 -0400

    https://leetcode.com/problems/merge-strings-alternately/ - easy

--- src/LeetCode/easy/Problem1768.java
+++ src/LeetCode/easy/Problem1768.java
@@ -8,9 +8,31 @@ public class Problem1768 {
         System.out.println(mergeAlternately("abcd", "pq"));
     }
 
+    //3min
+    //Runtime
+    //1
+    //ms
+    //Beats
+    //82.95%
+    //Analyze Complexity
+    //Memory
+    //41.70
+    //MB
+    //Beats
+    //71.53%
+    public static String mergeAlternately(String word1, String word2) {
+        int a = 0, b = 0;
+        StringBuilder sb = new StringBuilder();
+        while (a < word1.length() || b < word2.length()) {
+            if (a < word1.length()) sb.append(word1.charAt(a++));
+            if (b < word2.length()) sb.append(word2.charAt(b++));
+        }
+        return sb.toString();
+    }
+
     //Runtime: 1 ms, faster than 93.06% of Java online submissions for Merge Strings Alternately.
     //Memory Usage: 42.6 MB, less than 36.40% of Java online submissions for Merge Strings Alternately.
-    static String mergeAlternately(String word1, String word2) {
+    static String mergeAlternately1(String word1, String word2) {
         int min = Math.min(word1.length(), word2.length());
         String maxString = word1.length() > word2.length() ? word1 : word2;
         StringBuilder sb = new StringBuilder();

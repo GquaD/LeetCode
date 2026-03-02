689dbd8e2a835d8b9cca3a45cab9aee243380150
gquad <tbekpro@gmail.com>
2026-03-01 19:05:21 -0500

    https://leetcode.com/problems/minimum-cost-to-split-into-ones med

---
+++ src/LeetCode/medium/Problem3857.java
@@ -0,0 +1,23 @@
+package LeetCode.medium;
+
+public class Problem3857 {
+    //2min
+    //Runtime
+    //0
+    //ms
+    //Beats
+    //100.00%
+    //Analyze Complexity
+    //Memory
+    //42.50
+    //MB
+    //Beats
+    //100.00%
+    public static void main(String[] args) {
+
+    }
+
+    public int minCost(int n) {
+        return n * (n - 1) / 2;
+    }
+}

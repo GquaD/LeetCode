24f7675e9086fcd741d59f1d0bd6be4089b3d96a
gquad <tbekpro@gmail.com>
2026-03-05 08:14:30 -0500

    https://leetcode.com/problems/final-element-after-subarray-deletions/ med

---
+++ src/LeetCode/medium/Problem3828.java
@@ -0,0 +1,19 @@
+package LeetCode.medium;
+
+public class Problem3828 {
+    //10min
+    //Runtime
+    //0
+    //ms
+    //Beats
+    //100.00%
+    //Memory
+    //113.47
+    //MB
+    //Beats
+    //97.53%
+    public int finalElement(int[] nums) {
+        return Math.max(nums[0], nums[nums.length - 1]);
+    }
+
+}

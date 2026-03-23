d0912037930f91ca55c32f926f35053629a88ed7
gquad <tbekpro@gmail.com>
2026-03-23 08:00:06 -0400

    https://leetcode.com/problems/construct-uniform-parity-array-i/ easy

---
+++ src/LeetCode/easy/Problem3875.java
@@ -0,0 +1,19 @@
+package LeetCode.easy;
+
+public class Problem3875 {
+    //1min
+
+    //Runtime
+    //1
+    //ms
+    //Beats
+    //-%
+    //Memory
+    //45.18
+    //MB
+    //Beats
+    //33.33%
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}

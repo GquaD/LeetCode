e34d9a2f5a394155e139c0680d5502084e834366
gquad <tbekpro@gmail.com>
2026-09-04 12:13:10 -0500

    https://leetcode.com/problems/longest-substring-without-repeating-characters med

--- src/LeetCode/medium/Problem3.java
+++ src/LeetCode/medium/Problem3.java
@@ -1,5 +1,6 @@
 package LeetCode.medium;
 
+import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -10,6 +11,35 @@ public class Problem3 {
         System.out.println(lengthOfLongestSubstring("abcabcbb"));
         System.out.println(lengthOfLongestSubstring("bbbbb"));
         System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+    //Runtime
+    //27
+    //ms
+    //Beats
+    //53.68%
+    //Memory
+    //47.24
+    //MB
+    //Beats
+    //60.39%
+    public int lengthOfLongestSubstring2(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int len = 0, max = 0, repeatIdx = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            Integer lastIdx = map.get(c);
+            if (lastIdx == null) {
+                len++;
+            } else {
+                max = Math.max(len, max);
+                repeatIdx = Math.max(repeatIdx, lastIdx);
+                len = i - repeatIdx;
+            }
+            map.put(c, i);
+        }
+        return Math.max(len, max);
     }
 
     //https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/2625638/Java-Bad-Solution

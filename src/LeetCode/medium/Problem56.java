3d01f5a0fcd5faa7c4cdab3bfa49ccec9d25cc07
gquad <tbekpro@gmail.com>
2026-09-12 11:23:59 -0500

    https://leetcode.com/problems/merge-intervals med

--- src/LeetCode/medium/Problem56.java
+++ src/LeetCode/medium/Problem56.java
@@ -2,8 +2,10 @@ package LeetCode.medium;
 
 import LeetCode.LeetCodeUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class Problem56 {
     //https://leetcode.com/problems/merge-intervals/
@@ -21,6 +23,47 @@ public class Problem56 {
                 {1,4},
                 {2,3}
         }));
+    }
+
+
+    //15min
+    //Runtime
+    //8
+    //ms
+    //Beats
+    //90.11%
+    //Memory
+    //48.95
+    //MB
+    //Beats
+    //74.71%
+    public int[][] merge2(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
+
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int curStart = intervals[i][0], curEnd = intervals[i][1];
+            int[] last = list.get(list.size() - 1);
+            if (prevEnd >= curStart) {
+                if (curEnd <= prevEnd) continue;
+                if (curEnd > prevEnd) last[1] = curEnd;
+            } else {
+                list.add(intervals[i]);
+            }
+
+            prevEnd = curEnd;
+        }
+
+        int[][] result = new int[list.size()][2];
+
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
     }
 
     //https://leetcode.com/problems/merge-intervals/solutions/2832657/java-95-time-96-memory-solution/

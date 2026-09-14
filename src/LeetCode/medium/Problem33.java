f91160aa247121cefe10766d1ef289fd46a5ed25
gquad <tbekpro@gmail.com>
2026-09-14 13:42:08 -0500

    https://leetcode.com/problems/search-in-rotated-sorted-array med

--- src/LeetCode/medium/Problem33.java
+++ src/LeetCode/medium/Problem33.java
@@ -15,6 +15,42 @@ public class Problem33 {
         System.out.println(search(new int[]{1}, 0));
     }
 
+    //15min
+    //Runtime
+    //0
+    //ms
+    //Beats
+    //100.00%
+    //Memory
+    //43.71
+    //MB
+    //Beats
+    //65.74%
+    public int search3(int[] nums, int target) {
+        //find the start point with binary search
+        //then use binary search on both parts
+
+        if (nums[0] < nums[nums.length - 1]) {
+            int idx = Arrays.binarySearch(nums, target);
+            return idx < 0 ? -1 : idx;
+        }
+
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[nums.length - 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        int idx1 = Arrays.binarySearch(nums, 0, left + 1, target), idx2 = Arrays.binarySearch(nums, right, nums.length, target);
+        if (idx1 < 0 && idx2 < 0) return -1;
+
+        return idx1 >= 0 ? idx1 : idx2;
+    }
+
     //https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/2662980/java-o-n-log-n-solution-explained-79-faster/
     //
     //15 mins

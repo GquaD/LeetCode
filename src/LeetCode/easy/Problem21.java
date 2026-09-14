dbab401770afa7869fe33a698deb747767c814a4
gquad <tbekpro@gmail.com>
2026-09-14 12:02:12 -0500

    https://leetcode.com/problems/merge-two-sorted-lists easy

--- src/LeetCode/easy/Problem21.java
+++ src/LeetCode/easy/Problem21.java
@@ -6,6 +6,53 @@ public class Problem21 {
 
     }
 
+    //15min
+    //Runtime
+    //0
+    //ms
+    //Beats
+    //100.00%
+    //Memory
+    //44.45
+    //MB
+    //Beats
+    //19.87%
+    public ListNode mergeTwoLists3(ListNode list1, ListNode list2) {
+        ListNode head, temp;
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        if (list1.val <= list2.val) {
+            head = list1;
+            list1 = list1.next;
+            head.next = null;
+        } else {
+            head = list2;
+            list2 = list2.next;
+            head.next = null;
+        }
+
+        temp = head;
+
+        while (!(list1 == null || list2 == null)) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+            temp.next = null;
+        }
+
+        temp.next = list1 == null ? list2 : list1;
+
+        return head;
+    }
+
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode result;
         if (list1 == null && list2 == null) {

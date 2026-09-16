9212705c498d77d80a38040aff407be58e054082
gquad <tbekpro@gmail.com>
2026-09-16 09:41:09 -0500

    https://leetcode.com/problems/valid-sudoku med

--- src/LeetCode/medium/Problem36.java
+++ src/LeetCode/medium/Problem36.java
@@ -1,6 +1,8 @@
 package LeetCode.medium;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Problem36 {
@@ -8,6 +10,64 @@ public class Problem36 {
 
     public static void main(String[] args) {
 
+    }
+
+
+    //10-15min
+    //Runtime
+    //3
+    //ms
+    //Beats
+    //46.67%
+    //Memory
+    //46.20
+    //MB
+    //Beats
+    //91.55%
+    public boolean isValidSudoku2(char[][] board) {
+        //9 sets for horizontal
+        //9 sets for vertical
+        //9 sets for squares
+
+        List<Set<Character>> hor = createSets(), ver = createSets(), sq = createSets();
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char ch = board[r][c];
+                if (ch != '.') {
+                    boolean h = hor.get(r).add(ch);
+                    boolean v = ver.get(c).add(ch);
+                    boolean s = sq.get(findSquare(r, c)).add(ch);
+
+                    if (!(h && v && s)) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<Set<Character>> createSets() {
+        List<Set<Character>> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) list.add(new HashSet<>());
+        return list;
+    }
+
+    private int findSquare(int row, int col) {
+        int square = -1;
+        if (row <= 2) {
+            if (col <= 2) square = 0;
+            else if (col <= 5) square = 1;
+            else square = 2;
+        } else if (row <= 5) {
+            if (col <= 2) square = 3;
+            else if (col <= 5) square = 4;
+            else square = 5;
+        } else {
+            if (col <= 2) square = 6;
+            else if (col <= 5) square = 7;
+            else square = 8;
+        }
+
+        return square;
     }
 
     //5-10 more minutes

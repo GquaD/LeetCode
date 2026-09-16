d2796818ba2863ba5d791680fd4a6d50bce36c39
gquad <tbekpro@gmail.com>
2026-09-16 13:19:08 -0500

    https://leetcode.com/problems/trapping-rain-water hard

--- src/LeetCode/hard/Problem42.java
+++ src/LeetCode/hard/Problem42.java
@@ -1,5 +1,7 @@
 package LeetCode.hard;
 
+import java.util.Stack;
+
 public class Problem42 {
     //https://leetcode.com/problems/trapping-rain-water/
     public static void main(String[] args) {
@@ -7,6 +9,38 @@ public class Problem42 {
         System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
         System.out.println(trap(new int[]{4,2,0,3,2,5}));
     }
+
+    //nnn
+    //Runtime
+    //6
+    //ms
+    //Beats
+    //4.21%
+    //Memory
+    //48.44
+    //MB
+    //Beats
+    //29.56%
+    public int trap3(int[] height) {
+        int result = 0, curIdx = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        while (curIdx < height.length) {
+            while (!stack.isEmpty() && height[curIdx] > height[stack.peek()]) {
+                int last = stack.pop();
+
+                if (stack.isEmpty()) break;
+
+                int distance = curIdx - stack.peek() - 1, h = Math.min(height[curIdx], height[stack.peek()]) - height[last];
+                result += distance * h;
+            }
+            stack.push(curIdx++);
+        }
+
+        return result;
+    }
+
 
     //nnn
     //Runtime

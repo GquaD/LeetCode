a91524234e70c89977eef57040236d7601819b80
gquad <tbekpro@gmail.com>
2026-09-17 14:33:27 -0500

    https://leetcode.com/problems/rotate-image medium

--- src/LeetCode/medium/Problem48.java
+++ src/LeetCode/medium/Problem48.java
@@ -11,6 +11,33 @@ public class Problem48 {
         });
     }
 
+
+    //with help
+    //Runtime
+    //0
+    //ms
+    //Beats
+    //100.00%
+    //Memory
+    //43.78
+    //MB
+    //Beats
+    //52.92%
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        if (n == 1) return;
+
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+
     //https://leetcode.com/problems/rotate-image/discuss/2575946/Java-Solution
     //Runtime: 1 ms, faster than 9.22% of Java online submissions for Rotate Image.
     //Memory Usage: 43 MB, less than 27.23% of Java online submissions for Rotate Image.
